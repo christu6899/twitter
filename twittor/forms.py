74309df4b566f180de2a638bccd827f44bcf8a32
christu6899 <worktu6899@gmail.com>
2022-06-30 20:54:44 +0800

    version 0.5
    Signed-off-by: christu6899 <worktu6899@gmail.com>

--- twittor/forms.py
+++ twittor/forms.py
@@ -1,7 +1,8 @@
+from curses.ascii import EM
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
+from wtforms.validators import DataRequired,Email,EqualTo,ValidationError
+from twittor.models import User
 class LoginForm(FlaskForm):
     class Meta:
         csrf = False
@@ -9,3 +10,20 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password",validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username",validators=[DataRequired()])
+    email = StringField("Email Address", validators=[DataRequired(),Email()])
+    password = PasswordField("Password",validators=[DataRequired()])
+    password2 = PasswordField("Password Repeat",validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('please use differnet username')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('please use differnet email address')
